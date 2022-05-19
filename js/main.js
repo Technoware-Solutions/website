86c499a7fa505c5d4dd654e03e01df5cf3135846
caiogbarreira <rm94526@fiap.com.br>
2022-05-19 09:07:26 -0300

    * corrigido formularios * icones adicionados * botao logout

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 function showPassword(){
     var x = document.getElementById("senha");
+    var btn = document.getElementById("showPasswordBtn");
     if (x.type === "password") {
         x.type = "text";
+        btn.setAttribute("class", "showPassword icon show-on")
     } else {
         x.type = "password";
+        btn.setAttribute("class", "showPassword icon show-off")
     }
 }
